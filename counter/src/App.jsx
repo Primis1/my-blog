feb92b9f858f795fc756d5458b4f1524efc8d291
Primis1 <laleyla3361@gmail.com>
2023-12-26 23:02:10 -0500

    okay, i know i know how to work with html and css in jsx files

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,24 +1,31 @@
 import { useState } from 'react'
+import myLogo from './assets/logo.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <section className='block'>
+
+        <a href="https://github.com/Primis1" target='__blank'>
+          <img src={ myLogo } className='logo' alt="" href='https://github.com/Primis1'/>
+        </a>
+
+        <h1>My Counter</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            First react Project
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </section>
   )
 }
 
