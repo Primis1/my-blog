7b72126dca0de37a7c19dfc7b5ccf2670ec7d19e
Primis1 <laleyla3361@gmail.com>
2023-12-29 14:16:44 -0500

    this project is done, but waiting for new features

--- counter/src/button.jsx
+++ counter/src/button.jsx
@@ -1,12 +1,25 @@
-
+import { useState } from 'react';
 import './button.css'
 import './index.css'
+import App from './App.jsx';
 
 function Btn() {
+    const [open, setOpen] = useState(false);
+
     return (
-        
-        <button>Begin count</button>
-    )
+        <body>
+            {!open && <button onClick={ () => setOpen(true)}>Begin count</button>}
+            {open && <App />}
+        </body>
+
+        // <>
+        // <button onClick={ () => setOpen(true)}>Begin count</button>
+
+        // {open && < App/>}
+        // </>
+            )
 }
 
 export default Btn;
+
+
--- counter/src/main.jsx
+++ counter/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import Btn from './button.jsx' // import your second component here
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
     <Btn /> 
+    {/* <App /> */}
   </React.StrictMode>,
 )
